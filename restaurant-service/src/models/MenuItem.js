9e57ff1524a15452254fd38aa0ee9fcb70407475
Jayani Perera <pererajayani27@gmail.com>
2026-04-22 14:23:41 +0530

    Revert "Add resturent codes"

--- restaurant-service/src/models/MenuItem.js
+++
@@ -1,7 +0,0 @@
-const menuItemSchema = new mongoose.Schema({
-  restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant' },
-  name: String,
-  price: Number,
-  isAvailable: { type: Boolean, default: true }
-});
-module.exports = mongoose.model('MenuItem', menuItemSchema);
